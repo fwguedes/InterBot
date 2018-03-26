f52718c8eb8dce7fb387be7c8040ec471c8dce35
Filipe Guedes <fguedes@stone.com.br>
2018-03-26 00:01:09 -0300

    Adicionada novas etapas e classe de Agendamento

---
+++ InterBot/Agendamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterBot
+{
+    public class Agendamento
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Cidade { get; set; }
+        public string Acomodação { get; set; }
+        public string Escola { get; set; }
+        public string Tempo { get; set; }
+        public string Adicionais { get; set; }
+        public string VisitaNaAgencia { get; set; }
+    }
+}
--- InterBot/Program.cs
+++ InterBot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Telegram.Bot;
 
@@ -11,7 +13,9 @@ namespace InterBot
     {
         private static readonly TelegramBotClient bot = new TelegramBotClient("596056551:AAHF_u5VlopPp_v91WVnl-ng5ilGL4UuA9Q");
 
-        private static Dictionary<int,string> UserSteps = new Dictionary<int, string>();
+        private static Dictionary<int, string> UserSteps = new Dictionary<int, string>();
+
+        private static Agendamento agendamento;
 
 
         static void Main(string[] args)
@@ -31,49 +35,185 @@ namespace InterBot
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
 
-
-
-
-
-                if(UserSteps.ContainsKey(e.Message.From.Id) && UserSteps[e.Message.From.Id] == "Cidade")
+                //Contato Inicial ou Reiniciando o bot
+                if (e.Message.Text.Contains("/start"))
                 {
-                    if (Cities().Find(c => c.Contains(e.Message.Text) ) != null)
+                    Send(e, "Ola " + e.Message.From.Username + "\n Escolha uma cidade");
+                    Send(e, Cities());
+                    if (!UserSteps.ContainsKey(e.Message.From.Id))
                     {
-                        Send(e, e.Message.From.Username + "\n Escolha uma acomodação");
-                        Send(e, Houses());
-                        UserSteps[e.Message.From.Id] = "Acomodacao";
+                        UserSteps.Add(e.Message.From.Id, "Cidade");
                     }
                     else
                     {
-                        Send(e, "Comando Inválido. Escolha uma das cidades");
-                        Send(e, Cities());
+                        UserSteps[e.Message.From.Id] = "Cidade";
+                    }
+
+                    agendamento = new Agendamento();
+                    agendamento.Id = Guid.NewGuid();
+                    agendamento.Nome = e.Message.From.Username;
+
+                }
+                else
+                {
+                    switch (UserSteps[e.Message.From.Id])
+                    {
+
+                        case "Cidade":
+
+
+                            if (Cities().Find(c => c.Contains(e.Message.Text)) != null)
+                            {
+                                agendamento.Cidade = e.Message.Text;
+
+                                Send(e, e.Message.From.Username + "\n Escolha uma acomodação");
+                                Send(e, Houses());
+                                UserSteps[e.Message.From.Id] = "Acomodação";
+
+                            }
+                            else
+                            {
+                                Send(e, "Comando Inválido. Escolha uma das cidades");
+                                Send(e, Cities());
+                            }
+
+                            break;
+
+                        case "Acomodação":
+
+
+                            if (Houses().Find(c => c.Contains(e.Message.Text)) != null)
+                            {
+                                agendamento.Acomodação = e.Message.Text;
+
+                                Send(e, e.Message.From.Username + "\n Escolha uma escola");
+                                Send(e, Schools());
+                                UserSteps[e.Message.From.Id] = "Escola";
+
+                            }
+                            else
+                            {
+                                Send(e, "Comando Inválido. Escolha uma das acomodações");
+                                Send(e, Houses());
+                            }
+
+                            break;
+
+                        case "Escola":
+
+
+                            if (Schools().Find(c => c.Contains(e.Message.Text)) != null)
+                            {
+                                agendamento.Escola = e.Message.Text;
+                                
+                                Send(e, e.Message.From.Username + "\n Escolha uma dos adicionais");
+                                Send(e, Adds());
+                                UserSteps[e.Message.From.Id] = "Adicional";
+
+                            }
+                            else
+                            {
+                                Send(e, "Comando Inválido. Escolha uma das escolas");
+                                Send(e, Schools());
+                            }
+
+                            break;
+
+                        case "Adicional":
+
+
+                            if (Adds().Find(c => c.Contains(e.Message.Text)) != null)
+                            {
+                                agendamento.Adicionais = e.Message.Text;
+
+                                Send(e, e.Message.From.Username + "\n Digite seu tempo de permanencia em dias, meses ou anos");
+                                UserSteps[e.Message.From.Id] = "Tempo";
+
+                            }
+                            else
+                            {
+                                Send(e, "Comando Inválido. Escolha uma dos adicionais");
+                                Send(e, Adds());
+                            }
+
+                            break;
+
+                        case "Tempo":
+
+
+                            if (IsValidTime(e.Message.Text))
+                            {
+                                agendamento.Tempo = e.Message.Text;
+
+                                Send(e, "Solicitação de Intercambio\n-Cidade: " + agendamento.Cidade + "\n-Acomodação: " + agendamento.Acomodação + "\n-Escola: " + agendamento.Escola + "\n-Adicional: " + agendamento.Adicionais + "\n-Tempo de Permanencia: " + agendamento.Tempo);
+                                Send(e, e.Message.From.Username + "\n Confirmar Solicitação ?(Sim/Não)");
+                                UserSteps[e.Message.From.Id] = "Confirmação";
+
+                            }
+                            else
+                            {
+                                Send(e, "Comando Inválido. Digite seu tempo de permanencia em dias, meses ou anos");
+                                Send(e, Adds());
+                            }
+
+                            break;
+
+                        case "Confirmação":
+
+
+                            if (e.Message.Text == "Sim")
+                            {
+                                Send(e, "Diga quando você virá a agencia");
+                                UserSteps[e.Message.From.Id] = "Agendamento";
+
+                            }
+                            else if (e.Message.Text == "Não")
+                            {
+                                agendamento = new Agendamento();
+                                agendamento.Id = Guid.NewGuid();
+                                agendamento.Nome = e.Message.From.Username;
+
+                                Send(e, "Reiniciando...Escolha a cidade");
+                                Send(e, Cities());
+                                UserSteps[e.Message.From.Id] = "Cidade";
+
+                                
+
+                            }
+                            else
+                            {
+                                Send(e, "Comando Inválido. Confirmar Solicitação ?(Sim/Não)");
+                            }
+
+                            break;
+
+
+
+                        case "Agendamento":
+
+                            if (IsValidDateTimeTest(e.Message.Text))
+                            {
+                                Send(e, "Visita Agendada!");
+                                UserSteps[e.Message.From.Id] = "Finalizado";
+                                agendamento.VisitaNaAgencia = e.Message.Text;
+
+                            }
+                            else
+                            {
+                                Send(e, "Data Invalida");
+
+                            }
+
+                            break;
+
+
+
+                        default:
+                            break;
                     }
                 }
-
-
-
-
-
-
-
-
-                if (e.Message.Text.Contains("/start"))
-                {
-                    Send(e, "Ola " +  e.Message.From.Username + "\n Escolha uma cidade");
-                    Send(e, Cities());
-                    UserSteps.Add(e.Message.From.Id, "Cidade");
-                }
-
-
-
-
-
-                
-
-              
             }
         }
-
 
 
         public static void Send(Telegram.Bot.Args.MessageEventArgs e, string msg)
@@ -106,6 +246,43 @@ namespace InterBot
                 "Flat"
             };
         }
+
+        public static List<string> Schools()
+        {
+            return new List<string>()
+            {
+                "Internetional House - Premium",
+                "Evanz - Normal",
+                "Thompsom - Budget"
+            };
+        }
+
+        public static List<string> Adds()
+        {
+            return new List<string>()
+            {
+                "Transfer",
+                "Workshop",
+                "Nenhum"
+            };
+        }
+
+
+        public static bool IsValidTime(string time)
+        {
+            var regex = new Regex("[0-9]* [a-z|A-Z]*");
+            return regex.Match(time).Success;
+
+        }
+
+        public static bool IsValidDateTimeTest(string dateTime)
+        {
+            string[] formats = { "dd/MM/yyyy" };
+            DateTime parsedDateTime;
+            return DateTime.TryParseExact(dateTime, formats, new CultureInfo("pt-BR"),
+                                           DateTimeStyles.None, out parsedDateTime);
+        }
+
 
     }
 
